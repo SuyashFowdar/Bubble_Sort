442d5297dd5de2e97ac53fe7bcd30538a35c66ae
Azeez <mrkhunmie@gmail.com>
2020-11-16 15:25:17 +0100

    code refactoring

--- app.rb
+++ app.rb
@@ -1,18 +1,20 @@
+# frozen_string_literal: true
+
 def bubble_sort(arr)
-    done = false
-    while done == false
-        done = true
-        arr.each_with_index do |item, index|
-            if !arr[index + 1].nil? and arr[index + 1] < arr[index]
-                a = arr[index]
-                b = arr[index + 1]
-                arr[index + 1] = a
-                arr[index]  = b
-                done = false
-            end
-        end
+  done = false
+  while done == false
+    done = true
+    arr.each_with_index do |_item, index|
+      next unless !arr[index + 1].nil? && (arr[index + 1] < arr[index])
+
+      a = arr[index]
+      b = arr[index + 1]
+      arr[index + 1] = a
+      arr[index] = b
+      done = false
     end
-    puts(arr)
+  end
+  puts(arr)
 end
 
 array = [2, 4, 1, 6, 3, 7]
