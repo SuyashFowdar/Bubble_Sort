2cd6d1a70e73e638bd920d2edc529246e86a9eda
Azeez <mrkhunmie@gmail.com>
2020-11-16 15:28:35 +0100

    added bubblesort code refactored

--- app.rb
+++ app.rb
@@ -20,3 +20,28 @@ end
 array = [2, 4, 1, 6, 3, 7]
 
 bubble_sort(array)
+
+
+
+def bubble_sort_by(arr)
+  done = false
+  while done == false
+    done = true
+    arr.each_with_index do |_item, index|
+      next if arr[index + 1].nil?
+
+      a = arr[index]
+      b = arr[index + 1]
+      next unless yield(a, b).positive?
+
+      arr[index + 1] = a
+      arr[index] = b
+      done = false
+    end
+  end
+  puts(arr)
+end
+bubble_sort_by(%w[hi hello hey]) do |left, right|
+  left.length - right.length
+end
+
