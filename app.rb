93236bac8d7798e82ff7858e6b775e78b3cf9458
Suyash <suyash.krishna9@gmail>
2020-11-16 16:34:24 +0400

    Created first function

---
+++ app.rb
@@ -0,0 +1,15 @@
+arr = [2, 4, 1, 6, 3, 7]
+done = false
+while done == false
+    done = true
+    arr.each_with_index do |item, index|
+        if !arr[index + 1].nil? and arr[index + 1] < arr[index]
+            a = arr[index]
+            b = arr[index + 1]
+            arr[index + 1] = a
+            arr[index]  = b
+            done = false
+        end
+    end
+end
+puts(arr)
