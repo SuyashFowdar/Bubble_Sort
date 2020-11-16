da389ed5b008cbbc9d7ed2a9c3069518fc6e1c68
Suyash <suyash.krishna9@gmail>
2020-11-16 16:40:27 +0400

    Added sort as function with argument

--- app.rb
+++ app.rb
@@ -1,15 +1,20 @@
-arr = [2, 4, 1, 6, 3, 7]
-done = false
-while done == false
-    done = true
-    arr.each_with_index do |item, index|
-        if !arr[index + 1].nil? and arr[index + 1] < arr[index]
-            a = arr[index]
-            b = arr[index + 1]
-            arr[index + 1] = a
-            arr[index]  = b
-            done = false
+array = [2, 4, 1, 6, 3, 7]
+
+bubble_sort(array)
+
+def bubble_sort(arr)
+    done = false
+    while done == false
+        done = true
+        arr.each_with_index do |item, index|
+            if !arr[index + 1].nil? and arr[index + 1] < arr[index]
+                a = arr[index]
+                b = arr[index + 1]
+                arr[index + 1] = a
+                arr[index]  = b
+                done = false
+            end
         end
     end
+    puts(arr)
 end
-puts(arr)
